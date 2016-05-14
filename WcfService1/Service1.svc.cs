555e6c34644cd947821e18c955360cd0cf9bce51
sujeet kumar <developer20sujeet@gmail.com>
2016-05-14 14:40:30 +0530

    Initial commit.

---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1 : ServiceBase, IService1
+    {
+        private readonly ILog log;
+        public Service1() : base()
+        {
+            //Get logger for Service1
+            log = LogManager.GetLogger(typeof(Service1));
+        }
+
+        public string GetData(int value)
+        {
+            log.Info("Entering GetValue()");
+            
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ WcfService1/ServiceBase.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class ServiceBase
+    {
+        private readonly ILog log;
+        public ServiceBase()
+        {
+            //Load log4net Configuration
+            XmlConfigurator.Configure();
+            //Get logger
+            log = LogManager.GetLogger(typeof(ServiceBase));
+            //Start logging
+            log.Debug("ServiceBase Constructor Call");
+           
+
+        }
+    }
+}
